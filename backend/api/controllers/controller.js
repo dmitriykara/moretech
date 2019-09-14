f170bba70dd7850192038d111646b1e674deb62b
Kara Dmitriy <karadmitrii@gmail.com>
2019-09-15 01:41:28 +0300

    Add new controllers

--- backend/api/controllers/controller.js
+++ backend/api/controllers/controller.js
@@ -2,11 +2,57 @@
 
 const mongoose = require('mongoose');
 const User = mongoose.model('Users');
+const Poll = mongoose.model('Polls');
+const Answer = mongoose.model('Answers');
 
-exports.get_users = function (req, res) {
+exports.getUsers = (req, res) => {
   User.find({}, (err, user) => {
     if (err)
       res.send(err);
     res.json(user);
+  });
+}
+
+exports.getPolls = (req, res) => {
+  Poll.find({}, (err, user) => { //ToDo check for user's poll
+    if (err)
+      res.send(err);
+    res.json(user);
+  });
+}
+
+exports.addPoll = (req, res) => {
+  Poll.insertMany([req.poll], (err, polls) => {
+    if (err)
+      res.send(err);
+  });
+}
+
+exports.updatePoll = (req, res) => {
+  Poll.update({"id":req.poll.id}, req.poll, (err, poll) => {
+    if (err)
+      res.send(err);
+    res.send("OK")
+  });
+}
+
+exports.addAnswer = (req, res) => {
+  Answer.insertMany([req.answer], (err, answer) => {
+    if (err)
+      req.send(err);
+    Poll.update(
+      {"id":req.answer.pollid}, 
+      {"$addToSet": {"answers": req.answer.id}}, 
+      (err, poll) => {
+        if (err)
+          res.send(err);
+      });
+  });
+}
+
+exports.updateAnswer = (req, res) => {
+  Answer.update({"id":req.answer.id}, req.answer, (err, answer) => {
+    if (err)
+      res.send(err);
   });
 }
