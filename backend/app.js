d01d1ace5198880c79515af611523c898ef38e6a
skalniy <skalniy@inbox.ru>
2019-09-14 22:04:35 +0300

    add static serving

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,10 @@
 'use strict';
 
+const api = require('./api/routes/routes');
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
+
 
 mongoose.connect('mongodb://localhost/moretech', {
   useNewUrlParser: true,
@@ -11,13 +14,12 @@ mongoose.connection.on(
   'error',
   console.error.bind(console, 'connection error:')
 );
-mongoose.connection.once('open', function () {
-  console.log('db connected');
-});
+mongoose.connection.once('open', () => console.log('db connected'));
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use('/api', api);
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`listening on port ${port}`));
