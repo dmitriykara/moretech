f555bbca14d99c28375475009ef82c9fa618ac98
Kara Dmitriy <karadmitrii@gmail.com>
2019-09-14 23:16:29 +0300

    Update routes

--- backend/api/routes/routes.js
+++ backend/api/routes/routes.js
@@ -4,6 +4,14 @@ module.exports = function (app) {
   const controller = require('../controllers/controller');
 
   app.route('/users')
-    .get(controller.get_users)
-  //.post(controller)
+    .get(controller.getUsers)
+  
+  app.route('/polls')
+    .get(controller.getPolls)
+    .post(controller.addPoll)
+    .put(controller.updatePoll)
+
+  app.route('/answers')
+    .post(controller.addAnswer)
+    .put(controller.updateAnswer)
 }
