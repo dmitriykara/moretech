fa1df8290d363882c34152d8aea801fc892a6ad9
skalniy <skalniy@inbox.ru>
2019-09-14 20:02:10 +0300

    add mongo

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3000
+'use strict';
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const express = require('express');
+const mongoose = require('mongoose');
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+mongoose.connect('mongodb://localhost/my_database', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+mongoose.connection.on(
+  'error',
+  console.error.bind(console, 'connection error:')
+);
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
