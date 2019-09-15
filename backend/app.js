94b78d9b0dcef728bf86f6b6872d1f0448b947be
skalniy <skalniy@inbox.ru>
2019-09-15 11:02:26 +0300

    add req param readers

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,14 @@
 const path = require('path');
 
 const express = require('express');
-const mongoose = require('mongoose');
 
-const Poll = require('./api/models/poll');
-const User = require('./api/models/user');
+const mongoose = require('mongoose');
+mongoose.set('useCreateIndex', true);
+let _ = require('./api/models/poll');
+_ = require('./api/models/pollExpiration')
+_ = require('./api/models/user');
 
 const api = require('./api/routes/routes');
-
 
 mongoose.connect('mongodb://127.0.0.1:27017/moretech', {
   useNewUrlParser: true,
@@ -23,6 +24,9 @@ mongoose.connection.once('open', () => console.log('db connected'));
 
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', api);
 app.use(express.static(path.join(__dirname, 'public')));
