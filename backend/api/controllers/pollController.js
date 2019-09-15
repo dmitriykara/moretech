d86c5d3dcdee8049286d0a1046dd9f6915e24642
skalniy <skalniy@inbox.ru>
2019-09-15 07:05:53 +0300

    refactor controller

--- backend/api/controllers/pollController.js
+++ backend/api/controllers/pollController.js
@@ -5,48 +5,65 @@ const Poll = mongoose.model('Poll');
 //const Answer = mongoose.model('Answer');
 
 exports.getPolls = (req, res) => {
-  Poll.find({ partitipants: req.user.id }, (err, user) => {
-    if (err)
-      res.send(err);
+  Poll.find({ partitipants: req.login }, (err, user) => {
+    if (err) res.send(err);
+
     res.json(user);
   });
 }
 
 exports.addPoll = (req, res) => {
-  Poll.insertMany([req.poll], (err, polls) => {
-    if (err)
-      res.send(err);
+  Poll.create(req.poll, (err, poll) => {
+    if (err) res.send(err);
+
     res.sendStatus(200);
   });
 }
 
 exports.updatePoll = (req, res) => {
-  Poll.update({ id: req.poll.id }, req.poll, (err, poll) => {
-    if (err)
-      res.send(err);
+  Poll.updateOne({ id: req.poll.id }, req.poll, (err, poll) => {
+    if (err) res.send(err);
+
     res.sendStatus(200);
   });
 }
 
 exports.addAnswer = (req, res) => {
-  Answer.insertMany([req.answer], (err, answer) => {
-    if (err)
-      req.send(err);
-    Poll.update(
-      { id: req.answer.pollid },
-      { $addToSet: { answers: req.answer.id } },
-      (err, poll) => {
-        if (err)
-          res.send(err);
-        res.sendStatus(200);
-      });
+  // Answer.create([req.answer], (err, answer) => {
+  //   if (err) req.send(err);
+  //   Poll.update(
+  //     { id: req.answer.pollid },
+  //     { $addToSet: { answers: req.answer.id } },
+  //     (err, poll) => {
+  //       if (err) res.send(err);
+  //       res.sendStatus(200);
+  //     });
+  // });
+  Poll.findById(req.pollId, 'answers', (err, poll) => {
+    if (err) res.send(err);
+
+    poll.answers.create(req.answer, (err, doc) => {
+      if (err) res.send(err);
+
+      res.sendStatus(200);
+    });
   });
 }
 
 exports.updateAnswer = (req, res) => {
-  Answer.update({ id: req.answer.id }, req.answer, (err, answer) => {
-    if (err)
-      res.send(err);
-    res.sendStatus(200);
+  // Answer.update({ id: req.answer.id }, req.answer, (err, answer) => {
+  //   if (err)
+  //     res.send(err);
+  //   res.sendStatus(200);
+  // });
+  Poll.findById(req.pollId, 'answers', (err, poll) => {
+    if (err) res.send(err);
+    const answer = poll.answers.id(req.answer.id);
+
+    answer.update(req.answer, (err, doc) => {
+      if (err) res.send(err);
+
+      res.sendStatus(200);
+    });
   });
 }
