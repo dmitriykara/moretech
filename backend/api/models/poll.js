ae7bb40b4b12e3671068c0bfb75ba8fa9d31952c
skalniy <skalniy@inbox.ru>
2019-09-15 07:53:49 +0300

    add status to poll

--- backend/api/models/poll.js
+++ backend/api/models/poll.js
@@ -13,13 +13,14 @@ const PollSchema = new mongoose.Schema({
   createdAt: { type: Date, default: Date.now },
   deadline: Date,
   description: String,
-  title: String,
   partitipants: [
     {
       login: String,
       isRequired: Boolean,
     }
   ],
+  status: { type: String, enum: ['completed', 'open', 'precompleted'] },
+  title: String,
 });
 
 module.exports = mongoose.model('Poll', PollSchema);
